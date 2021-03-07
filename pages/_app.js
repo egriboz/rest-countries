1930c4c33083a35efc5b21125753d384c94ca01b
fatih egriboz <fatih.egriboz@bilgi.edu.tr>
2021-03-07 23:12:23 +0300

    add CssReset

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import theme from "../theme";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <CSSReset />
       <Component {...pageProps} />
     </ChakraProvider>
   );
