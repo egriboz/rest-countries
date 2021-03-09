60b4c696511b376fbf25ee2e0be310e41af2e6af
Fatih Egriboz <fatih.egriboz@bilgi.edu.tr>
2021-03-09 15:09:32 +0300

    chakra serverprops back <-

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
 import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import theme from "../theme";
-import { Chakra } from "../components/Chakra";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <Chakra cookies={pageProps.cookies}>
-        <CSSReset />
-        <Component {...pageProps} />
-      </Chakra>
+      <CSSReset />
+      <Component {...pageProps} />
     </ChakraProvider>
   );
 }
-export { getServerSideProps } from "../components/Chakra";
