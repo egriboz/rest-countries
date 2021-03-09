a083f7b89af7122d623c9019e088d913fac1fe91
Fatih Egriboz <fatih.egriboz@bilgi.edu.tr>
2021-03-09 13:26:30 +0300

    dark mode initial set

--- theme.js
+++ theme.js
@@ -3,10 +3,12 @@
 import { extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 // 2. Add your color mode config
+
 const config = {
-  initialColorMode: "light",
+  initialColorMode: "dark",
   useSystemColorMode: false,
 };
+
 // 3. extend the theme
 const theme = extendTheme({
   config,
